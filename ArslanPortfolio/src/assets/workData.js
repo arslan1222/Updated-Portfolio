5fb08a4b4531c4cef2aa4b93445b9a0d8eb51d3d
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-04-19 15:36:11 +0500

    2 new projects added

--- ArslanPortfolio/src/assets/workData.js
+++ ArslanPortfolio/src/assets/workData.js
@@ -1,74 +1,88 @@
-import wImage1 from "./doc_appointment.png";
-import wImage2 from "./ecommerce_app.png";
-import wImage3 from "./food_app.png";
-import wImage4 from "./wanderlust.png";
-import wImage5 from "./real_estate.png";
-import wImage6 from "./college_app.png";
-import wImage7 from "./weather_forcasting.png";
-import wImage8 from "./youtube_clone.png";
-import wImage9 from "./spotify.png";
-import wImage10 from "./blog_app.png";
+import wImage1 from "./lms.png";
+import wImage2 from "./grocery.png";
+import wImage3 from "./doc_appointment.png";
+import wImage4 from "./ecommerce_app.png";
+import wImage5 from "./food_app.png";
+import wImage6 from "./wanderlust.png";
+import wImage7 from "./real_estate.png";
+import wImage8 from "./college_app.png";
+import wImage9 from "./weather_forcasting.png";
+import wImage10 from "./youtube_clone.png";
+import wImage11 from "./spotify.png";
+import wImage12 from "./blog_app.png";
 
 
 export const workData = [
   {
     wNo: 1,
-    wName: "Doctor Appointment App",
+    wName: "LMS Project",
     wImage: wImage1,
-    wLink: "https://how-i-grow-doctor-appointment.onrender.com",
+    wLink: "https://edemy-skills-lms.onrender.com/",
   },
   {
     wNo: 2,
-    wName: "E-commerce App",
+    wName: "Grocery Website",
     wImage: wImage2,
-    wLink: "https://e-commerce-connect.onrender.com",
+    wLink: "https://grocery-your-need.onrender.com",
   },
   {
     wNo: 3,
-    wName: "Food Delivery App",
+    wName: "Doctor Appointment App",
     wImage: wImage3,
-    wLink: "https://cooking-food-delivery.onrender.com",
+    wLink: "https://how-i-grow-doctor-appointment.onrender.com",
   },
   {
     wNo: 4,
-    wName: "Wanderlust Travel Site",
+    wName: "E-commerce App",
     wImage: wImage4,
-    wLink: "https://a-z-wanderlust.onrender.com/listings",
+    wLink: "https://e-commerce-connect.onrender.com",
   },
   {
     wNo: 5,
-    wName: "Real Estate App",
+    wName: "Food Delivery App",
     wImage: wImage5,
-    wLink: "https://real-estate-properties.onrender.com",
+    wLink: "https://cooking-food-delivery.onrender.com",
   },
   {
     wNo: 6,
-    wName: "College App",
+    wName: "Wanderlust Travel Site",
     wImage: wImage6,
-    wLink: "https://zaroon-international-university.onrender.com",
+    wLink: "https://a-z-wanderlust.onrender.com/listings",
   },
   {
     wNo: 7,
-    wName: "Weather Forecasting App",
+    wName: "Real Estate App",
     wImage: wImage7,
-    wLink: "https://my-weather-api-6byz.onrender.com/signup",
+    wLink: "https://real-estate-properties.onrender.com",
   },
   {
     wNo: 8,
-    wName: "Youtube Clone",
+    wName: "College App",
     wImage: wImage8,
-    wLink: "https://youtube-clone-reactjs.onrender.com",
+    wLink: "https://zaroon-international-university.onrender.com",
   },
   {
     wNo: 9,
-    wName: "Spotify Clone",
+    wName: "Weather Forecasting App",
     wImage: wImage9,
-    wLink: "https://spotify-clone-reactjs.onrender.com",
+    wLink: "https://my-weather-api-6byz.onrender.com/signup",
   },
   {
     wNo: 10,
-    wName: "Blog Application",
+    wName: "Youtube Clone",
     wImage: wImage10,
+    wLink: "https://youtube-clone-reactjs.onrender.com",
+  },
+  {
+    wNo: 11,
+    wName: "Spotify Clone",
+    wImage: wImage11,
+    wLink: "https://spotify-clone-reactjs.onrender.com",
+  },
+  {
+    wNo: 12,
+    wName: "Blog Application",
+    wImage: wImage12,
     wLink: "https://howigrow-blogs.onrender.com",
   },
 ];
